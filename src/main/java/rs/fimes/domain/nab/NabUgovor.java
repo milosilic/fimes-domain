96fd53d4b144726f54885704191251b4fa373872
Milos Ilic  HP <milos.ilic@gmail.com>
2014-08-31 12:56:41 +0200

    nabUgovor

---
+++ src/main/java/rs/fimes/domain/nab/NabUgovor.java
@@ -0,0 +1,84 @@
+package rs.fimes.domain.nab;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+import rs.fimes.domain.FimesDomain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the nab_ugovor database table.
+ * 
+ */
+@Entity
+@Table(name="nab_ugovor")
+public class NabUgovor extends FimesDomain  implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer idUgovor;
+
+    private NabJavnaNabavka nabJavnaNabavka;
+    private String interniBroj;
+    private Date datumZakljucenja;
+
+    public NabUgovor() {
+    }
+
+    @Id
+    @Column(name="id_ugovor", unique=true, nullable=false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nab_ugovor")
+    @SequenceGenerator(name = "seq_nab_ugovor", sequenceName = "seq_nab_ugovor", initialValue = 1, allocationSize = 1)
+    public Integer getIdUgovor() {
+        return this.idUgovor;
+    }
+
+    public void setIdUgovor(Integer idUgovor) {
+        this.idUgovor = idUgovor;
+    }
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="id_javna_nabavka", nullable = false)
+    public NabJavnaNabavka getNabJavnaNabavka() {
+        return this.nabJavnaNabavka;
+    }
+
+    public void setNabJavnaNabavka(NabJavnaNabavka nabJavnaNabavka) {
+        this.nabJavnaNabavka = nabJavnaNabavka;
+    }
+
+    @Column(name="interni_broj", length=128)
+    public String getInterniBroj() {
+        return interniBroj;
+    }
+
+    public void setInterniBroj(String interniBroj) {
+        this.interniBroj = interniBroj;
+    }
+
+    @Temporal(TemporalType.DATE)
+    @Column(name="datum_zakljucenja", nullable=false)
+    public Date getDatumZakljucenja() {
+        return datumZakljucenja;
+    }
+
+    public void setDatumZakljucenja(Date datumZakljucenja) {
+        this.datumZakljucenja = datumZakljucenja;
+    }
+
+    @Override
+    public String toString() {
+        return "NabUgovor [idUgovor=" + idUgovor + ", nabJavnaNabavka="
+                + nabJavnaNabavka + ", interniBroj=" + interniBroj
+                + ", datumZakljucenja=" + datumZakljucenja + "]";
+    }
+
+    
+    
+    
+
+}
